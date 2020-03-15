bca1121f0d828a70111f62ec70ee9d18f5a5848f
Flaxen <alexander.carlsson@live.se>
2020-03-15 22:53:53 +0100

    xy to diag coord working

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -44,16 +44,25 @@ public class playerScript : MonoBehaviour
 
     Vector2 toDiagCoord(Vector2 vector)
     {
-        float x = vector.x;
-        float y = vector.y;
+        float a = vector.x;
+        float b = vector.y;
 
-        float diagX = y - (0.5f * x);   // x point on classic x axis according to isometric coord sys
-        float diagY = (0.5f * x) - y;   // x point on classic y axis according to isometric coord sys
+        // rotated coord sys has x-axis = 1/2 x and y-axis = -1/2 x
 
-        float diagXcoord = Mathf.Sqrt(Mathf.Pow(diagX, 2) + Mathf.Pow(0.5f * diagX, 2));   // fully converted x coordinate according to 45 degree rotated coord sys
-        float diagYcoord = Mathf.Sqrt(Mathf.Pow(diagY, 2) + Mathf.Pow(-0.5f * diagY, 2));  // fully converted y coordinate according to 45 degree rotated coord sys
+        float diagXcoord = Mathf.Sqrt(Mathf.Pow((0.5f * a + b), 2) + Mathf.Pow((0.25f * (a + 2 * b)), 2));   // fully converted x coordinate according to rotated coord sys
+        float diagYcoord = Mathf.Sqrt(Mathf.Pow((0.5f * a - b), 2) + Mathf.Pow((0.25f * (-a + 2 * b)), 2));  // fully converted y coordinate according to rotated coord sys
 
-        return new Vector2(diagXcoord, diagYcoord);
+        return new Vector2(diagXcoord%1, diagYcoord%1);
+    }
+
+    Vector2 toXyCoord(Vector2 vector)
+    {
+        vector = toDiagCoord(vector);
+
+        float x = vector.y * -1;
+        float y = vector.x;
+
+        return new Vector2(x, y);
     }
 
     public void move(Vector2 tilePos)
@@ -62,14 +71,14 @@ public class playerScript : MonoBehaviour
         tilePos = toDiagCoord(tilePos);                         // translate to diagonal coordinates
         Vector2 playerPos = toDiagCoord(transformers.position); // translate player pos to diagonal coordinates
 
-        Vector2 distanceVector = tilePos - playerPos;
+        //Vector2 distanceVector = tilePos - playerPos;
 
-        Debug.Log(distanceVector);
+        //Debug.Log(tilePos);
 
 
         // TODO: change other way around! think 45 deg sys and convert to classic xy
         //tilePos = new Vector2(diagXcoord%1 + 0.5f, diagYcoord%1 + 0.5f);
 
-        //transformers.position = tilePos;
+        transformers.position = toXyCoord(tilePos);
     }
 }
